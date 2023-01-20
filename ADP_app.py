4fadcaa02bf5d0d5339e55426ba7f7943949c16e
inuwamobarak <inuwamobarak@gmail.com>
2023-01-20 23:05:13 +0100

    project upload

---
+++ ADP_app.py
@@ -0,0 +1,82 @@
+import io
+from PIL import Image
+import streamlit as st
+import torch
+from torchvision import transforms
+import base64
+
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:images/{"jpg"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('images/bg2.jpg')    
+
+MODEL_PATH = 'model/model-APD-pytorch-res50-92accuracy.pt'
+LABELS_PATH = 'model/model_classes.txt'
+
+
+def load_image():
+    uploaded_file = st.file_uploader(label='Pick a banknote to test')
+    if uploaded_file is not None:
+        image_data = uploaded_file.getvalue()
+        st.image(image_data)
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
+
+
+def load_model(model_path):
+    model = torch.load(model_path, map_location='cpu')
+    model.eval()
+    return model
+
+
+def load_labels(labels_file):
+    with open(labels_file, "r") as f:
+        categories = [s.strip() for s in f.readlines()]
+        return categories
+
+
+def predict(model, categories, image):
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    input_tensor = preprocess(image)
+    input_batch = input_tensor.unsqueeze(0)
+
+    with torch.no_grad():
+        output = model(input_batch)
+
+    probabilities = torch.nn.functional.softmax(output[0], dim=0)
+
+    all_prob, all_catid = torch.topk(probabilities, len(categories))
+    for i in range(all_prob.size(0)):
+        st.write(categories[all_catid[i]], all_prob[i].item())
+
+
+def main():
+    st.title('Colombian Pesu banknote Detection')
+    model = load_model(MODEL_PATH)
+    categories = load_labels(LABELS_PATH)
+    image = load_image()
+    result = st.button('Predict image')
+    if result:
+        st.write('Checking...')
+        predict(model, categories, image)
+
+
+if __name__ == '__main__':
+    main()
---
+++ APD_app_TF.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import tensorflow as tf
+import streamlit as st
+
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+  model=tf.keras.models.load_model('/content/my_model2APD.hdf5')
+  return model
+with st.spinner('Model is being loaded..'):
+  model=load_model()
+
+st.write("""
+         # Flower Classification
+         """
+         )
+
+file = st.file_uploader("Please upload an brain scan file", type=["jpg", "png"])
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+st.set_option('deprecation.showfileUploaderEncoding', False)
+def import_and_predict(image_data, model):
+    
+        size = (180,180)    
+        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+        image = np.asarray(image)
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        #img_resize = (cv2.resize(img, dsize=(75, 75),    interpolation=cv2.INTER_CUBIC))/255.
+        
+        img_reshape = img[np.newaxis,...]
+    
+        prediction = model.predict(img_reshape)
+        
+        return prediction
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    predictions = import_and_predict(image, model)
+    score = tf.nn.softmax(predictions[0])
+    st.write(prediction)
+    st.write(score)
+    print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
+
