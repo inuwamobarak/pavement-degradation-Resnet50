f937a382e87118972910a3d3addb86d427d7da24
Inuwa Mobarak Abraham <inuwamobarak@gmail.com>
2023-01-20 23:40:06 +0100

    Update ADP_app.py

--- ADP_app.py
+++ ADP_app.py
@@ -49,8 +49,8 @@ def load_labels(labels_file):
 
 def predict(model, categories, image):
     preprocess = transforms.Compose([
-        transforms.Resize(224),
-        transforms.CenterCrop(224),
+        transforms.Resize(300),
+        transforms.CenterCrop(300),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
