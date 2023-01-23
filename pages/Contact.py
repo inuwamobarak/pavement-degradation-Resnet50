f57581c7c779c6fefcb20b634c67c53ae4cf3b42
Inuwa Mobarak Abraham <inuwamobarak@gmail.com>
2023-01-23 20:22:18 +0100

    Update Contact.py

--- pages/Contact.py
+++ pages/Contact.py
@@ -1,1 +1,37 @@
+from PIL import Image
+import streamlit as st
+import base64
 
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:images/{"jpg"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('images/engineer.jpg')    
+
+def main():
+    st.title('Asphalt Pavement Degradation Detector')
+    html_text = '<p style="font-family:sans-serif; color:"Navy Blue"; font-size:16px; font-style:italic;"> <i>Welcome to the contact page. To keep intouch or for enquiries please find contacts below.</i></p>'
+    st.markdown(html_text, unsafe_allow_html=True)
+    
+    html_text = '<p style="font-family:sans-serif; color:"Navy Blue"; font-size:16px; font-style:italic;"> <i>inuwamobarak@gmail.com</i></p>'
+    st.markdown(html_text, unsafe_allow_html=True)
+    
+    
+    button('Predict image')
+    if result:
+        st.write('Checking...')
+        predict(model, categories, image)
+
+
+if __name__ == '__main__':
+    main()
